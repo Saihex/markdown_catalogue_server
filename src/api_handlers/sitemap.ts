5a0ffed56d2687392f07e308e4526318719c2d05
iskandaralex2 <iskandaralexfixed@gmail.com>
2025-05-18 23:19:21 +0800

    improved sitemap

--- src/api_handlers/sitemap.ts
+++ src/api_handlers/sitemap.ts
@@ -9,45 +9,95 @@ const exclusions: string[] = [
 ];
 
 export async function sitemap_api_handler(
-  _req: Request,
+  req: Request,
   _url: URL,
 ): Promise<Response> {
   // searching logic
   {
-    const dirRead = Deno.readDir(collection_path);
+    const url = new URL(req.url);
+    const directory = url.searchParams.get("wiki") || collection_path;
+    const layer0 = url.searchParams.get("wiki") == null;
+
+    let searchDir;
+
+    if (layer0 == true) {
+      searchDir = collection_path;
+    } else {
+      searchDir = collection_path + "/" + directory;
+    }
+
+    const dirRead = Deno.readDir(searchDir);
     const results: string[] = [];
 
     for await (const dirEntry of dirRead) {
       if (!exclusions.includes(dirEntry.name)) {
-        results.push(`https://wiki.saihex.com/wiki/${dirEntry.name}`);
+        if (layer0) {
+          results.push(
+            `https://wiki.saihex.com/api/msc/sitemap?wiki=${dirEntry.name}/category`,
+          );
+        } else {
+          results.push(
+            `https://wiki.saihex.com/wiki/${directory}/${dirEntry.name}`,
+          );
+        }
       }
     }
 
     const lastmod = new Date().toISOString().split("T")[0];
 
-    const sitemap_text = generateSitemap(results, lastmod);
+    let sitemap_text;
+    
+    if (layer0) {
+      sitemap_text = generateSitemap_layer0(results, lastmod);
+    } else {
+      sitemap_text = generateSitemap_layer1(results, lastmod);
+    }
 
     return new Response(sitemap_text, {
       status: 200,
       headers: {
         "Content-Type": "application/xml",
-        "Cache-Control": "no-cache"
+        "Cache-Control": "no-cache",
       },
     });
   }
 }
 
-function generateSitemap(urls: string[], lastmod: string): string {
+function escapeXml(str: string): string {
+  return str.replace(/&/g, "&amp;")
+            .replace(/</g, "&lt;")
+            .replace(/>/g, "&gt;")
+            .replace(/"/g, "&quot;")
+            .replace(/'/g, "&apos;");
+}
+
+// wiki listing
+function generateSitemap_layer0(urls: string[], lastmod: string): string {
   const sitemapHeader = '<?xml version="1.0" encoding="UTF-8"?>\n' +
     '<sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n';
   const sitemapFooter = "</sitemapindex>";
 
-  // Create XML for each sitemap entry
   const sitemapEntries = urls
     .map((url) => {
-      return `  <sitemap>\n    <loc>${url}</loc>\n    <lastmod>${lastmod}</lastmod>\n  </sitemap>`;
+      return `  <sitemap>\n    <loc>${escapeXml(url)}</loc>\n    <lastmod>${lastmod}</lastmod>\n  </sitemap>`;
     })
     .join("\n");
 
   return sitemapHeader + sitemapEntries + "\n" + sitemapFooter;
 }
+
+// wiki's category listing
+function generateSitemap_layer1(urls: string[], lastmod: string): string {
+  const sitemapHeader = '<?xml version="1.0" encoding="UTF-8"?>\n' +
+    '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n';
+  const sitemapFooter = "</urlset>";
+
+  const sitemapEntries = urls
+    .map((url) => {
+      return `  <url>\n    <loc>${escapeXml(url)}</loc>\n    <lastmod>${lastmod}</lastmod>\n    <changefreq>weekly</changefreq>\n  </url>`;
+    })
+    .join("\n");
+
+  return sitemapHeader + sitemapEntries + "\n" + sitemapFooter;
+}
+
